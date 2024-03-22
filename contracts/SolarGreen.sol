d94e35e61d321af6d74c7f9a0bff5032beee3901
nahirniy <nahirniyy@gmail.com>
2024-03-22 19:19:38 +0200

    initially commit && create token base

---
+++ contracts/SolarGreen.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SolarGreen is ERC20, ERC20Burnable, Ownable {
+    constructor(
+        address initialOwner
+    ) ERC20("Solar Green", "SGR") Ownable(initialOwner) {
+        _mint(msg.sender, 100000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
